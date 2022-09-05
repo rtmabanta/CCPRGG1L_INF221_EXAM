05608ebacbe9d3d85b61dfa2baab0747ed9af9a7
rtmabanta <111472801+rtmabanta@users.noreply.github.com>
2022-09-05 10:23:06 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+   
+    String firstName;
+    char middlename;
+    String surname;
+    String dateOfBirth;
+    int studentNumber;
+    String emailAddress;
+    boolean iAmAwesome;
+
+
+    public void sayMyStudentNumber() {
+        System.out.println("My student number " + studentNumber);
+    }
+    public void sayMyEmailAddress() {
+        System.out.println("My email address " + emailAddress);
+    }
+    public void sayamIAwesome() {
+        System.out.println("am I Awesome: " + iAmAwesome);
+    }
+}
