9491a2d8682254e4b5d615f7108dcbf0ba5a6de0
rtmabanta <111472801+rtmabanta@users.noreply.github.com>
2022-09-05 10:22:29 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Student Student = new Student();
+
+        Student.firstName = "Ristian";
+        Student.middlename = 'T';
+        Student.surname = "Mabanta";
+        Student.dateOfBirth = "11/16/1979";
+        Student.studentNumber = 2022102602;
+        Student.emailAddress = "mabantart@students.national-u.edu.ph";
+        Student.iAmAwesome = true;
+
+        Student.sayMyStudentNumber();
+        Student.sayMyEmailAddress();
+        Student.sayamIAwesome();  
+    }
+}
